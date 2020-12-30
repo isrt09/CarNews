cb47260f8b2f367383e76d5d7e267694b011c5f6
isrt09 <mazedur@gmail.com>
2020-12-31 03:12:22 +0600

    Template Segmentation

---
+++ wp-content/themes/carnews/appointment.php
@@ -0,0 +1,45 @@
+ <div id="appiontment-now-box">
+        <p class="toggle">APPOINTMENT</p>
+        <div class="appiontment-now ">
+            <div class="appiontment-form">
+                <p>Instant online Appiontment</p>
+                <form name="contact_us_popup" method="post" action="contact.php">
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <div class="form-group">
+                                <input type="text" placeholder="Enter Name" class="form-control required name" name="name" aria-required="true" id="name1" required>
+                            </div>
+                        </div>
+                        <div class="col-sm-6">
+                            <div class="form-group">
+                                <input type="email" placeholder="Email" class="form-control required email" name="email" aria-required="true" id="email" required>
+                            </div>
+                        </div>
+                        <div class="col-sm-6">
+                            <div class="form-group">
+                                <input name="form_appontment_date" class="form-control required date-picker" type="text" placeholder="Date" aria-required="true" id="datepicker1" required>
+                            </div>
+                        </div>
+                        <div class="col-sm-6">
+                            <div class="form-group">
+                                <input type="text" placeholder="Phone" class="form-control" name="phone" id="phone" required>
+                            </div>
+                        </div>
+
+                        <div class="col-sm-12">
+                            <div class="form-group">
+                                <input type="text" placeholder="Vehicle Registration Num* " class="form-control required" name="subject" id="reg1" required>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div class="form-group">
+                        <textarea placeholder="Message" rows="7" class="form-control required" name="message" required></textarea>
+                    </div>
+                    <div class="form-group tac">
+                        <button class="btn btn-primary transition7s" type="submit">APPOINTMENT Now</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
