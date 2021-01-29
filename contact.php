cccb0f07f78e1501adb583453cfd927ef574cc98
isrt09 <mazedur@gmail.com>
2021-01-30 03:31:17 +0600

    Custom Templates

--- contact.php
+++ contact.php
@@ -1,10 +1,14 @@
-<?php get_header(); ?>    
+<?php 
+/*
+Template Name:Contact US
+*/
+get_header(); ?>    
     <!-- News / Blog section  
     ============================================= -->
     <div id="news-area" class="section-gray news-section-single">
         <div class="container">
             <div class="row">
-                <div class="col-lg-8 col-md-8 col-sm-6 col-xs-12 ftl">
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 ftl">
                      <?php 
                         if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
                     <div class="single-page-details">
@@ -16,8 +20,7 @@
 					<?php endwhile; else : ?>
                     <p><?php _e( 'Sorry, no posts matched your criteria.', 'carnews' );  ?></p>
                     <?php endif; ?>                           
-                    </div>                    
-                    <?php get_sidebar(); ?>
+                    </div>                                        
                 </div>               
             </div>
         </div>
