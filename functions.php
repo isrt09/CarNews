d9817c6c8b9aeb3efbcffd0a8dc54fd5efd774a3
isrt09 <mazedur@gmail.com>
2021-02-01 00:33:22 +0600

    Slider Add

--- functions.php
+++ functions.php
@@ -10,20 +10,22 @@ function carnewsMenu(){
 }
 add_action('init','carnewsMenu');
 
-add_theme_support('post-thumbnails', array('post','page'));
+add_theme_support('post-thumbnails', array('post','page','gallery'));
 set_post_thumbnail_size(400,500,true);
+add_image_size('gallery',1900,900, true);
 
 
 
 function car_widgets(){
-		register_sidebar( array(
+	register_sidebar( array(
 			'name'          => __( 'Calender', 'carnews' ),
 			'id'            => 'widget-calender',
 			'before_widget' => '<div class="footer-widget">',
 			'after_widget'  => '</div>',
 			'before_title'  => '<h2 class="widget-title">',
 			'after_title'   => '</h2> ',
-		) );
+	) );
+
 		register_sidebar( array(
 		'name'          => __( 'Footer Widget-1', 'carnews' ),
 		'id'            => 'widget-footer-1',
@@ -67,3 +69,67 @@ function car_widgets(){
 
 }
 add_action( 'widgets_init', 'car_widgets' );
+
+
+add_action( 'show_user_profile', 'my_show_extra_profile_fields' );
+add_action( 'edit_user_profile', 'my_show_extra_profile_fields' );
+
+function my_show_extra_profile_fields( $user ) { ?>
+	<h3>Extra profile information</h3>
+	<table class="form-table">
+	    <tr>
+			<th><label for="facebook">Facebook</label></th>
+			<td>
+	      		    <input type="text" name="facebook" id="facebook" value="<?php echo esc_attr( get_the_author_meta( 'facebook', $user->ID ) ); ?>" class="regular-text" /><br />
+			     <span class="description">Please enter your facebook username. [e.g http://www.facebook.com/ruhul.phyi]</span>
+			</td>
+	   </tr>
+	   <tr>
+			<th><label for="facebook">Twitter</label></th>
+			<td>
+	      		    <input type="text" name="twitter" id="twitter" value="<?php echo esc_attr( get_the_author_meta( 'twitter', $user->ID ) ); ?>" class="regular-text" /><br />
+			     <span class="description">Please enter your twitter username. [e.g http://www.twitter.com/ruhul.phyi]</span>
+			</td>
+	   </tr>
+	   <tr>
+			<th><label for="youtube">YouTube</label></th>
+			<td>
+	      		    <input type="text" name="youtube" id="youtube" value="<?php echo esc_attr( get_the_author_meta( 'youtube', $user->ID ) ); ?>" class="regular-text" /><br />
+			     <span class="description">Please enter your youtube username. [e.g http://www.youtube.com/ruhul.phyi]</span>
+			</td>
+	   </tr>
+	</table>
+<?php }
+
+add_action( 'personal_options_update', 'my_save_extra_profile_fields' );
+add_action( 'edit_user_profile_update', 'my_save_extra_profile_fields' );
+
+function my_save_extra_profile_fields( $user_id ) {
+
+	if ( !current_user_can( 'edit_user', $user_id ) )
+		return false;
+
+		/* Copy and paste this line for additional fields. Make sure to change 'facebook' to the field ID. */
+		update_user_meta( $user_id, 'facebook', $_POST['facebook'] );
+		update_user_meta( $user_id, 'youtube',  $_POST['youtube'] );
+		update_user_meta( $user_id, 'twitter',  $_POST['twitter'] );
+	}
+
+function carnews_slider(){
+	register_post_type('gallery',[
+	'public' => true,
+		'labels' => array(
+			'name'    			=> __("Slider","carnews"),
+			'add_new' 			=> __("Add New","carnews"),
+			'add_new_item'  	=> __("Add New Slider","carnews"),
+			'edit_item'      	=> __("Edit Slider","carnews"),
+			'all_items'     	=> __("All Sliders","carnews"),
+			'featured_image'	=> __("Slider Image","carnews"),
+			'set_featured_image'=> __("Set Slider Image","carnews"),				
+		),
+		'supports'		=> array('title','editor','thumbnail'),
+		'menu_position'	=>10,
+		'menu_icon'     => 'dashicons-format-image'	
+	]);
+}
+add_action('init','carnews_slider');
--- slider.php
+++ slider.php
@@ -1,14 +1,23 @@
 <!--Main Slider-->
     <section id="home-slider-v1" class="slider-v1">
         <div class="main-slider-v1 owl-item">
-            <div class="item" style="background-image:url(<?php echo get_template_directory_uri(); ?>/images/resource/home-slide-1.jpg);">
+        <?php 
+            $carnews_slider = new WP_Query(array(
+                'post_type' => 'gallery'
+            ));
+
+            if($carnews_slider->have_posts()){
+                while($carnews_slider->have_posts()){
+                    $carnews_slider->the_post(); 
+                    $slider_image = wp_get_attachment_image_src(get_post_thumbnail_id(get_the_ID()),'full'); ?>
+            <div class="item" style="background-image: url(<?php echo $slider_image['0']?>);">
                 <div class="overlay"></div>
                 <div class="slide-description">
                     <div class="animate-item">
-                        <h2>Welcome to Our Car Care House!!!</h2>
+                        <?php the_title('<h2>','</h2>') ?>
                     </div>
                     <div class="animate-item">
-                        <h3>Top One Car Care in your city with very good quality Engr.</h3>
+                         <h3><?php the_content() ?></h3>
                     </div>
                     <div class="animate-item">
                         <a class="btn-1 btn btn-primary" href="#">Contact</a>
@@ -16,66 +25,9 @@
                     </div>
                 </div>
             </div>
-            <div class="item" style="background-image:url(<?php echo get_template_directory_uri(); ?>/images/resource/home-slide-2.jpg);">
-                <div class="overlay"></div>
-                <div class="slide-description">
-                    <div class="animate-item">
-                        <h2>Welcome to Our Car Care House!!!</h2>
-                    </div>
-                    <div class="animate-item">
-                        <h3>Top One Car Care in your city with very good quality Engr.</h3>
-                    </div>
-                    <div class="animate-item">
-                        <a class="btn-1 btn btn-primary" href="#">Contact</a>
-                        <a class="btn-2 btn btn-primary" href="#">Appoinment</a>
-                    </div>
-                </div>
-            </div>
-            <div class="item" style="background-image:url(<?php echo get_template_directory_uri(); ?>/images/resource/home-slide-3.jpg);">
-                <div class="overlay"></div>
-                <div class="slide-description">
-                    <div class="animate-item">
-                        <h2>Welcome to Our Car Care House!!!</h2>
-                    </div>
-                    <div class="animate-item">
-                        <h3>Top One Car Care in your city with very good quality Engr.</h3>
-                    </div>
-                    <div class="animate-item">
-                        <a class="btn-1 btn btn-primary" href="#">Contact</a>
-                        <a class="btn-2 btn btn-primary" href="#">Appoinment</a>
-                    </div>
-                </div>
-            </div>
-            <div class="item" style="background-image:url(<?php echo get_template_directory_uri(); ?>/images/resource/home-slide-4.jpg);">
-                <div class="overlay"></div>
-                <div class="slide-description">
-                    <div class="animate-item">
-                        <h2>Welcome to Our Car Care House!!!</h2>
-                    </div>
-                    <div class="animate-item">
-                        <h3>Top One Car Care in your city with very good quality Engr.</h3>
-                    </div>
-                    <div class="animate-item">
-                        <a class="btn-1 btn btn-primary" href="#">Contact</a>
-                        <a class="btn-2 btn btn-primary" href="#">Appoinment</a>
-                    </div>
-                </div>
-            </div>
-            <div class="item" style="background-image:url(<?php echo get_template_directory_uri(); ?>/images/resource/home-slide-5.jpg);">
-                <div class="overlay"></div>
-                <div class="slide-description">
-                    <div class="animate-item">
-                        <h2>Welcome to Our Car Care House!!!</h2>
-                    </div>
-                    <div class="animate-item">
-                        <h3>Top One Car Care in your city with very good quality Engr.</h3>
-                    </div>
-                    <div class="animate-item">
-                        <a class="btn-1 btn btn-primary" href="#">Contact</a>
-                        <a class="btn-2 btn btn-primary" href="#">Appoinment</a>
-                    </div>
-                </div>
-            </div>
+            <?php  }
+                }
+            ?>        
         </div>
     </section>
     <!-- Slider End -->
